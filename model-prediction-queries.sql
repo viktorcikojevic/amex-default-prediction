3d22117e86cb8f6a4b08d9d39eb575468b0ba318
viktorcikojevic <cikojevic.viktor@gmail.com>
2022-08-04 12:44:02 +0200

    added forkaggle prediction

--- model-prediction-queries.sql
+++ model-prediction-queries.sql
@@ -9,3 +9,16 @@ FROM
     FROM
       `big-query-study-338516.kaggle_amex_dataset.test_data_processed_3`
     ));
+
+
+create or replace table `big-query-study-338516.model_predictions.shut_up_and_calculate_model_balanced_100percent_processed_3_run1_ON_test_data_processed_3_forkaggle` as 
+select 
+customer_ID,
+MAX(prob) as prob
+from (
+select 
+customer_ID,
+pd.prob
+from 
+`big-query-study-338516.model_predictions.shut_up_and_calculate_model_balanced_100percent_processed_3_run1_ON_test_data_processed_3`, unnest(predicted_target_probs) as pd
+) group by customer_ID;
