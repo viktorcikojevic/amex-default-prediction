77c33666c46b11ab9b3c7b2db96b4fe2d0759696
viktorcikojevic <cikojevic.viktor@gmail.com>
2022-08-04 17:55:45 +0200

    processed 3: score 0.757

--- model-prediction-queries.sql
+++ model-prediction-queries.sql
@@ -13,12 +13,24 @@ FROM
 
 create or replace table `big-query-study-338516.model_predictions.shut_up_and_calculate_model_balanced_100percent_processed_3_run1_ON_test_data_processed_3_forkaggle` as 
 select 
-customer_ID,
-MAX(prob) as prob
+  customer_ID, 
+  CASE 
+    WHEN predicted_target = 0 and target_score < 0.61 THEN target_score
+    WHEN predicted_target = 0 and target_score > 0.61 THEN 1 - target_score
+    WHEN predicted_target = 1 and target_score < 0.61 THEN 1 - target_score
+    WHEN predicted_target = 1 and target_score > 0.61 THEN target_score
+  END as prediction
 from (
 select 
-customer_ID,
-pd.prob
-from 
-`big-query-study-338516.model_predictions.shut_up_and_calculate_model_balanced_100percent_processed_3_run1_ON_test_data_processed_3`, unnest(predicted_target_probs) as pd
-) group by customer_ID;
+customer_ID, predicted_target,
+MAX(prob) as target_score
+from (
+  select 
+  customer_ID,
+  pd.prob,
+  predicted_target
+  from 
+  `big-query-study-338516.model_predictions.shut_up_and_calculate_model_balanced_100percent_processed_3_run1_ON_test_data_processed_3`, unnest(predicted_target_probs) as pd
+) group by customer_ID, predicted_target
+)
+;
